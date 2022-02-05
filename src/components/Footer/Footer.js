3d9ba80d205ce4f10dd5827a80a55d3b3d16615e
Abu thakir <abudx.at@gmail.com>
2022-02-06 00:21:13 +0530

    contact added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ const footer = () => {
     return (
         <div className="footer">
             <div className="row m-0">
-                <div className="col-12 col-md-4 instagram">
+                <div className="col-12 col-md-3 instagram">
                     <div className="p-3">
                         <span className="d-block">
                             <i class="fa fa-instagram"></i>
@@ -13,7 +13,7 @@ const footer = () => {
                         <span className="reference"><i style={{ textDecoration: "underline" }}><a href="https://www.instagram.com/sunopticalssivakasi/" rel="noopener noreferrer" target="_blank">sunopticalssivakasi</a></i></span>
                     </div>
                 </div>
-                <div className="col-12 col-md-4 facebook">
+                <div className="col-12 col-md-3 facebook">
                     <div className="p-3">
                         <span className="d-block">
                             <i class="fa fa-facebook-f"></i>
@@ -21,12 +21,20 @@ const footer = () => {
                         <span className="reference"><i style={{ textDecoration: "underline" }}><a href="https://www.facebook.com/pages/category/Sunglasses---Eyewear-Store/Sun-Optical-Sivakasi-358552978187146/" rel="noopener noreferrer" target="_blank">Sun Optical</a></i></span>
                     </div>
                 </div>
-                <div className="col-12 col-md-4 location">
+                <div className="col-12 col-md-3 location">
                     <div className="p-3">
                         <span className="d-block">
                             <i class="fa fa-map-marker"></i>
                         </span>
                         <span className="reference"><i style={{ textDecoration: "underline" }}><a href="https://goo.gl/maps/tioPkUXiQhZmdtBd6" rel="noopener noreferrer" target="_blank">Location</a></i></span>
+                    </div>
+                </div>
+                <div className="col-12 col-md-3 contact">
+                    <div className="p-3">
+                        <span className="d-block">
+                            <i class="fa fa-phone"></i>
+                        </span>
+                        <span className="reference"><i style={{ textDecoration: "none" }}>+91-7708473942</i></span>
                     </div>
                 </div>
             </div>
